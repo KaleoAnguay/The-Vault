6ee8b086accc44c2dfdf21eb9c504d6cd3434e70
Michael <michaelanguay@outlook.com>
2017-03-15 15:11:42 -1000

    Finished vault project

--- vault.js
+++ vault.js
@@ -1,4 +1,22 @@
 'use strict';
-module.exports = function() {
-  
-};
+module.exports = (function() {
+  var vault = {};
+  var myKey = {};
+  function setValue(key,value){
+    vault = value;
+    myKey = key;
+  }
+
+  function getValue(key){
+    if(key === myKey) {
+      return vault;
+    } else {
+      return null;
+    }
+  }
+
+  return {
+    setValue,
+    getValue
+  };
+});
